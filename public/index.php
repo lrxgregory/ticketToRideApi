30a2960e6c0b86b20eca9a1b8ba2616294728dc7
Gregory Leroux <gregory.leroux.partner@decathlon.com>
2023-09-22 12:02:38 +0200

    redirect the root page to the api doc

--- public/index.php
+++ public/index.php
@@ -1,8 +1,15 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Response;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+
+if ($_SERVER['REQUEST_URI'] === '/') {
+    $response = new Response('', Response::HTTP_FOUND, ['Location' => '/api/doc']);
+    $response->send();
+    exit;
+}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
